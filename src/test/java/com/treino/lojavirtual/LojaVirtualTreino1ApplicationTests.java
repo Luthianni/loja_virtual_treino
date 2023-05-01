b691073e4973f2eab804cfd1c57e69b006a44033
Luthianni Alves <luthgoran@gmail.com>
2023-05-01 17:00:32 -0300

    Commit inicial projeto java spring.

---
+++ src/test/java/com/treino/lojavirtual/LojaVirtualTreino1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treino.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualTreino1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
